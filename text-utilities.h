1803ce62197228195bc80abf87257cae64f92625
Kurt Kartaltepe <kkartaltepe@gmail.com>
2018-04-18 19:25:25 -0500

    Update file handling, gradients
    Properly read entire files if not in chatlog mode
    Properly handle utf8/16 detection and BOMs
    Slightly nicer gradient shading
    Add user selectable file encoding support

--- text-utilities.h
+++ text-utilities.h
@@ -4,6 +4,7 @@
 #include <pango/pangocairo.h>
 #include <util/platform.h>
 #include <obs-module.h>
+#include <gmodule.h>
 
 
 #define RGBA_CAIRO(c) \
@@ -119,6 +120,20 @@ static bool pango_source_properties_gradient_changed(obs_properties_t *props,
 	return true;
 }
 
+static bool pango_source_properties_encoding_changed(obs_properties_t *props,
+		obs_property_t *property, obs_data_t *settings)
+{
+	UNUSED_PARAMETER(property);
+	obs_property_t *prop;
+
+	bool enabled = obs_data_get_bool(settings, "encoding.enable");
+
+	prop = obs_properties_get(props, "encoding.name");
+	obs_property_set_visible(prop, enabled);
+
+	return true;
+}
+
 static bool pango_source_properties_from_file_changed(obs_properties_t *props,
 		obs_property_t *property, obs_data_t *settings)
 {
@@ -149,66 +164,181 @@ static bool pango_source_properties_log_mode_changed(obs_properties_t *props,
 	return true;
 }
 
-static bool read_from_end(char **dst_buf, const char *filename, const int n_lines)
+char *encoding_ln[4] = {
+	"\n", // unknown just use single byte newline
+	"\n", // UTF-8
+	"\n\x00" // UTF-16LE
+	"\x00\n", // UTF-16BE
+};
+
+static bool read_from_end(char **dst_buf, size_t *size, uint8_t *utf_encoding, const char *filename, int lines)
 {
-	FILE *tmp_file = NULL;
+	FILE *file = NULL;
 	uint32_t filesize = 0, cur_pos = 0;
-	uint16_t value = 0, line_breaks = 0;
+	uint16_t line_breaks = 0;
 	size_t bytes_read;
-	char bvalue;
+
+	uint8_t encoding = 0;
+	char bom[4] = {0, 0, 0, 0};
+	size_t header_offset = 0;
+	size_t alignment = 1;
+	uint8_t bytes[2] = {0, 0};
 
 	bool utf16 = false;
 
-	tmp_file = fopen(filename, "rb");
-	if (tmp_file == NULL) {
+	file = os_fopen(filename, "rb");
+	if (file == NULL) {
 		blog(LOG_WARNING, "Failed to open file %s", filename);
 		return false;
 	}
-	bytes_read = fread(&value, 2, 1, tmp_file);
+	bytes_read = fread(bom, 1, 3, file);
 
-	if (bytes_read == 2 && (value == 0xFEFF || value == 0xFFFE))
+	if (bytes_read == 3 && strncmp(bom, "\xEF\xBB\xBF", 3) == 0) {
+		utf16 = false;
+		encoding = 1; // UTF-8
+		header_offset = 3;
+	} else if (bytes_read >= 2 && strncmp(bom, "\xFF\xFE", 2) == 0) {
 		utf16 = true;
+		encoding = 2; // UTF-16LE
+		header_offset = 2;
+		alignment = 2;
+	} else if (bytes_read >= 2 && strncmp(bom, "\xFE\xFF", 2) == 0) {
+		utf16 = true;
+		encoding = 3; // UTF-16BE
+		header_offset = 2;
+		alignment = 2;
+	}
 
-	fseek(tmp_file, 0, SEEK_END);
-	filesize = (uint32_t)ftell(tmp_file);
+	blog(LOG_WARNING, "enc: %d, ho: %d, align: %d", encoding, header_offset, alignment);
+
+	fseek(file, 0, SEEK_END);
+	filesize = (uint32_t)ftell(file);
 	cur_pos = filesize;
 
-	while (line_breaks <= n_lines && cur_pos != 0) {
-		if (!utf16) cur_pos--;
-		else cur_pos -= 2;
-		fseek(tmp_file, cur_pos, SEEK_SET);
+	while (line_breaks <= lines && cur_pos != 0) {
+		cur_pos -= alignment;
+		fseek(file, cur_pos, SEEK_SET);
 
-		if (!utf16) {
-			bytes_read = fread(&bvalue, 1, 1, tmp_file);
-			if (bytes_read == 1 && bvalue == '\n')
-				line_breaks++;
-		}
-		else {
-			bytes_read = fread(&value, 2, 1, tmp_file);
-			if (bytes_read == 2 && value == L'\n')
-				line_breaks++;
+		bytes_read = fread(bytes, 1, alignment, file);
+		if(bytes_read == alignment && strncmp(bytes, encoding_ln[encoding], alignment) == 0) {
+			line_breaks++;
 		}
 	}
 
+	// If we are not at the end shift past the newline, otherwise shift off the header
 	if (cur_pos != 0)
-		cur_pos += (utf16) ? 2 : 1;
+		cur_pos += alignment;
+	else
+		cur_pos += header_offset;
 
-	fseek(tmp_file, cur_pos, SEEK_SET);
+	fseek(file, cur_pos, SEEK_SET);
 
-	if (utf16) {
-		wchar_t *tmp_buf = bzalloc(filesize - cur_pos);
+	*dst_buf = bzalloc(filesize - cur_pos + 1);
+	*size = fread(*dst_buf, 1, filesize - cur_pos, file);
+	(*dst_buf)[filesize - cur_pos] = 0;
+	*utf_encoding = encoding;
 
-		bytes_read = fread(tmp_buf, (filesize - cur_pos), 1,
-				tmp_file);
-
-		os_wcs_to_utf8_ptr(tmp_buf, bytes_read, dst_buf);
-		bfree(tmp_buf);
-	} else {
-		*dst_buf = bzalloc(filesize - cur_pos + 1);
-		bytes_read = fread(*dst_buf, filesize - cur_pos, 1, tmp_file);
-		(*dst_buf)[filesize - cur_pos] = 0;
-	}
-	
-	fclose(tmp_file);
+	fclose(file);
 	return true;
+}
+
+static bool read_whole_file(char **dst_buf, size_t *size, uint8_t *utf_encoding, const char *filename) {
+	size_t len = 0;
+	char *tmp_buf = NULL;
+	char bom[4] = {0, 0, 0, 0};
+	uint8_t encoding = 0;
+	size_t header_offset = 0;
+
+	FILE *file = os_fopen(filename, "rb");
+	if(!file) {
+		blog(LOG_WARNING, "Failed to open file %s", filename);
+		return false;
+	}
+	len = fread(bom, 1, 3, file);
+
+	if (len == 3 && strncmp(bom, "\xEF\xBB\xBF", 3) == 0) {
+		encoding = 1; // UTF-8
+		header_offset = 3;
+	} else if (len >= 2 && strncmp(bom, "\xFF\xFE", 2) == 0) {
+		encoding = 2; // UTF-16LE
+		header_offset = 2;
+	} else if (len >= 2 && strncmp(bom, "\xFE\xFF", 2) == 0) {
+		encoding = 3; // UTF-16BE
+		header_offset = 2;
+	}
+
+	fseek(file, 0, SEEK_END);
+	len = (size_t)os_ftelli64(file);
+
+	if (len > 0) {
+		tmp_buf = bmalloc(len-header_offset+1);
+
+		fseek(file, header_offset, SEEK_SET);
+		len = fread(tmp_buf, 1, len-header_offset, file);
+		if (len == 0) {
+			bfree(tmp_buf);
+			fclose(file);
+			return false;
+		}
+	}
+	fclose(file);
+
+	*dst_buf = tmp_buf;
+	(*dst_buf)[len-header_offset] = 0;
+	*size = len-header_offset;
+	*utf_encoding = encoding;
+	return true;
+}
+
+static bool read_textfile(struct pango_source *src)
+{
+	char *tmp_buf = NULL;
+	size_t size = 0;
+	uint8_t	encoding_header = 0;
+
+	if(src->log_mode) {
+		if (!read_from_end(&tmp_buf, &size, &encoding_header, src->text_file, src->log_lines)) {
+			blog(LOG_WARNING, "Failed to read from end of file: %s", src->text_file);
+			return false;
+		}
+	} else {
+		if (!read_whole_file(&tmp_buf, &size, &encoding_header, src->text_file)) {
+			blog(LOG_WARNING, "Failed to read all of file: %s", src->text_file);
+			return false;
+		}
+	}
+
+	// Check UTF-8
+	if (g_utf8_validate(tmp_buf, size, NULL)) { // Very low false positive
+		src->text = tmp_buf;
+		return true;
+	}
+
+	char *encoding = NULL;
+	if (encoding_header == 2) {
+		encoding = "UTF-16LE";
+	} else if (encoding_header == 3) {
+		encoding = "UTF-16BE";
+	} else if (src->encoding) { // Only use user-defined encoding if we _havnt_ read valid utf-8 or a utf-16 bom.
+		encoding = src->encoding;
+	} else { // Use glib narrow locale if we have no other guesses, has no invalid encodings i presume.
+		g_get_charset(&encoding);
+	}
+
+	GError *err = NULL;
+	gsize conv_size = 0;
+	gchar *conv_str = g_convert (tmp_buf, size, "UTF-8", encoding, NULL, &conv_size, &err);
+	bfree(tmp_buf);
+	if (conv_str != NULL) {
+		src->text = bstrdup_n(conv_str, conv_size);
+		g_free(conv_str);
+		return true;
+	} else {
+		if (err)
+			blog(LOG_WARNING, "Failed to convert file from: %s, conversion ended at %d. Msg: %s", encoding, conv_size, err->message);
+		else
+			blog(LOG_WARNING, "Failed to convert file from: %s. Bad g_convert call", encoding);
+	}
+
+	return false;
 }
